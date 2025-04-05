dc7f0c5ceb5ed1b70679cfa4b0ed87084e016910
Krisztina <raczkrisztina91@gmail.com>
2025-04-05 16:11:06 +0200

    Fix: add module declarations for swiper css

---
+++ src/types/swiper-css.d.ts
@@ -0,0 +1,4 @@
+// Declare swiper CSS modules to silence TypeScript errors during build
+declare module 'swiper/css';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/pagination';
